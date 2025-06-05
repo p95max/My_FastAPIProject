cd00a83bd38263beb37398ff7e88569c358296db
maxx <maxpetrikin@gmail.com>
2025-06-05 09:46:13 +0200

    first commit

---
+++ database.py
@@ -0,0 +1,12 @@
+from sqlalchemy import engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
---
+++ main.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI, Depends
+from pydantic import BaseModel
+from database import get_db
+from models import Book
+
+app = FastAPI()
+
+class BookSchema(BaseModel):
+    id: int
+    title: str
+    author: str | None
+
+
+@app.get("/book/{id}")
+def get_book(id: int, db=Depends(get_db)):
+    return db.query(Book).get(id)
+
+
+@app.post("/add_book")
+def add_book(book: BookSchema, db=Depends(get_db)):
+    db.add(Book(**book.model_dump()))
+    db.commit()
+
+
+@app.delete("/book/{id}")
+def delete_book(id: int, db=Depends(get_db)):
+    db.query(Book).filter_by(id=id).delete()
+    db.commit()
+
+
+@app.get("/all_books")
+def get_all_books(db=Depends(get_db)):
+    return db.query(Book).all()
